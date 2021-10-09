a99929f374154d36f6136e7488e95780b9c44e87
Naoki Takezoe <takezoe@gmail.com>
2021-10-09 17:41:03 +0900

    Fix ESLint warnings

--- src/extension.ts
+++ src/extension.ts
@@ -9,30 +9,30 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-csv-markdown" is now active!')
+	console.log('Congratulations, your extension "vscode-csv-markdown" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('vscode-csv-markdown.convert-csv-to-markdown', () => {
-		const editor = vscode.window.activeTextEditor
-		if (editor == null) {
-			return
+		const editor = vscode.window.activeTextEditor;
+		if (editor === null || editor === undefined) {
+			return;
 		}
-		const selection = editor.selection
-		const text = editor.document.getText(selection)
-		const csv  = parse(text)
-		const rows = csv.data as Array<Array<String>>
+		const selection = editor.selection;
+		const text = editor.document.getText(selection);
+		const csv  = parse(text);
+		const rows = csv.data as Array<Array<String>>;
 
 		// Caluculate  column sizes
-		let width: number[] = []
+		let width: number[] = [];
 		rows.forEach((line, i) => {
 			line.forEach((element, j) => {
-				const value = element.trim()
+				const value = element.trim();
 				if (width.length <= j) {
-					width[j] = value.length < 4 ? 4 : value.length
+					width[j] = value.length < 4 ? 4 : value.length;
 				} else if (width[j] < value.length) {
-					width[j] = value.length
+					width[j] = value.length;
 				}
 			});
 		});
@@ -40,36 +40,36 @@ export function activate(context: vscode.ExtensionContext) {
 		// Build table
 		let table = "";
 		rows.forEach((line, i) => {
-			if (line.length == 1 && line[0].trim() == "") {
+			if (line.length === 1 && line[0].trim() === "") {
 				return;
 			}
 			line.forEach((element, j) => {
-				const value = element.trim()
-				table = table + "|" + value
+				const value = element.trim();
+				table = table + "|" + value;
 				if (value.length < width[j]) {
 					for (let k = 0; k < width[j] - value.length; k++) {
-						table = table + " "
+						table = table + " ";
 					}
 				}
 			});
-			table = table + "|\n"
-			if (i == 0) {
+			table = table + "|\n";
+			if (i === 0) {
 				for (const w of width) {
-					table = table + "|"
+					table = table + "|";
 					for (let k = 0; k < w; k++) {
-						table = table + "-"
+						table = table + "-";
 					}
 				}
-				table = table + "|\n"
+				table = table + "|\n";
 			}
 		});
 
 		editor.edit(function (editBuilder) {
-			editBuilder.replace(selection, table)
+			editBuilder.replace(selection, table);
   	});
 	});
 
-	context.subscriptions.push(disposable)
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
